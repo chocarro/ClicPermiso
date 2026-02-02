50fbbe78a885e65ec4bed8b7cf469b8ffd5b788b
chocarro <chocarroo@gmail.com>
2026-02-02 17:40:35 +0100

    main

--- clicpermiso/src/App.tsx
+++ clicpermiso/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './componentes/Layout'; 
 import FormSolicitud from './componentes/FormSolicitud';
 import MisAusencias from './componentes/MisAusencias';
 import PerfilForm from './componentes/PerfilForm';
@@ -7,15 +8,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-     
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/sol-diurno" />} />
+          
           <Route path="sol-diurno" element={<FormSolicitud />} />
           <Route path="sol-vespertino" element={<FormSolicitud />} />
-          
           <Route path="perfil" element={<PerfilForm />} />
-          
           <Route path="solicitados" element={<MisAusencias />} />
           <Route path="ausencias" element={<MisAusencias />} />
-          
+        </Route>
       </Routes>
     </BrowserRouter>
   );
