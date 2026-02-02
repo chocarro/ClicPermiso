3aa8fadd678ee6146c2fc5cb462bba5973d95e7e
chocarro <chocarroo@gmail.com>
2026-02-02 17:34:43 +0100

    +

--- clicpermiso/src/App.tsx
+++ clicpermiso/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout'; // Asegúrate de tener este archivo para el Sidebar
 import FormSolicitud from './componentes/FormSolicitud';
 import MisAusencias from './componentes/MisAusencias';
 import PerfilForm from './componentes/PerfilForm';
@@ -9,15 +8,11 @@ function App() {
     <BrowserRouter>
       <Routes>
      
-
-          {/* Rutas para los formularios de solicitud */}
           <Route path="sol-diurno" element={<FormSolicitud />} />
           <Route path="sol-vespertino" element={<FormSolicitud />} />
           
-          {/* Ruta para el perfil */}
           <Route path="perfil" element={<PerfilForm />} />
           
-          {/* Rutas para las ausencias y días solicitados */}
           <Route path="solicitados" element={<MisAusencias />} />
           <Route path="ausencias" element={<MisAusencias />} />
           
