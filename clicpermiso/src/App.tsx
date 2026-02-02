ea08effbde55a3d5e39bc190aaceab5918737246
chocarro <chocarroo@gmail.com>
2026-02-02 17:33:00 +0100

    App

--- clicpermiso/src/App.tsx
+++ clicpermiso/src/App.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout';
-import FormSolicitud from './pages/FormSolicitud';
-import PerfilForm from './pages/PerfilForm';
-import MisAusencias from './pages/MisAusencias';
+import Layout from './componentes/Layout'; 
+import FormSolicitud from './componentes/FormSolicitud';
+import MisAusencias from './componentes/MisAusencias';
+import PerfilForm from './componentes/PerfilForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/sol-diurno" />} />
+          
+          {/* Ambas rutas usan el MISMO componente FormSolicitud */}
+          <Route path="sol-diurno" element={<FormSolicitud />} />
+          <Route path="sol-vespertino" element={<FormSolicitud />} />
+          
+          <Route path="perfil" element={<PerfilForm />} />
+          <Route path="solicitados" element={<MisAusencias />} />
+          <Route path="ausencias" element={<MisAusencias />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
